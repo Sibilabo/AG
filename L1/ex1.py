58490c10b5303b358ef9fdc089327aca8b62d384
Sonia Gwizdek <sonia.gwizdek95@gmail.com>
2024-09-24 14:52:06 +0200

    learning about genetic algorithms

---
+++ L1/ex1.py
@@ -0,0 +1,3 @@
+# Zakoduj rozwiązanie problemu plecakowego jako chromosom (wektor binarny)
+# Przykład: Dla zestawu 5 przedmiotow o wartościach [10, 20, 30, 40, 50] i wagach [1, 2, 3, 8, 7], chromosom [1, 0, 1, 0, 1] oznacza, że w plecaku znajdują się przedmioty 1, 3, i 5.
+
